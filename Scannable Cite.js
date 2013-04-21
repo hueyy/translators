1e4b4ddd439ed8f9ab0e9485a416219a5da59671
Frank Bennett <biercenator@gmail.com>
2013-04-22 08:57:31 +0900

    Try out some changes for law

--- Scannable Cite.js
+++ Scannable Cite.js
@@ -15,21 +15,42 @@
 	"lastUpdated": "2013-04-16 11:10:08"
 }
 
+// legal types are weird
+var LEGAL_TYPES = ["legislation","legal_case","patent","bill","treaty","regulation"];
+var mem = new function () {
+    var isLegal = false;
+	var lst = [];
+    this.check = function (item) { isLegal = (LEGAL_TYPES.indexOf(item.type)>-1) };
+	this.set = function (str, slug) { if (str) {lst.push(str)} else if (!isLegal) {lst.push(slug)}};
+	this.setlaw = function (str, punc) { if (!punc) {punc = ""}; if (str && isLegal) {lst.push(str + punc)}};
+	this.get = function () { return lst.join(" ") };
+	this.reset = function () {lst = []};
+}
+
 function doExport() {
     var item;
     while (item = Zotero.nextItem()) {
-        Zotero.write("{|");
+        mem.reset();
+        Zotero.write("{ |");
         var library_id = item.LibraryID ? item.LibraryID : 0;
-        var titleS = (item.title) ? item.title : "(no title)";
-     	if (item.creators.length >0){
-  		var creatorsS = item.creators[0].lastName;
-        	if (item.creators.length > 2) creatorsS += " et al.";
-        	else if (item.creators.length == 2) creatorsS += " &amp; " + item.creators[1].lastName;
-	}
-	else var creatorsS = "anon.";
-        var date = Zotero.Utilities.strToDate(item.date);
+		mem.set(item.title,"(no title)");
+		if (item.creators.length >0){
+  			mem.set(item.creators[0].lastName);
+        	if (item.creators.length > 2) mem.set("et al.");
+        	else if (item.creators.length == 2) mem.set("&amp; " + item.creators[1].lastName);
+		}
+        else {
+			mem.set("anon.");
+        }
+        mem.setlaw(item.authority, ",");
+        mem.setlaw(item.volume);
+        mem.setlaw(item['container-title']);
+        mem.setlaw(item.page);
+    }
+	var date = Zotero.Utilities.strToDate(item.date);
         var dateS = (date.year) ? date.year : item.date;
         Zotero.write(creatorsS + ", " + titleS + ", " + dateS + "| | |");
         Zotero.write("zotero://select/items/" + library_id + "_" + item.key + "}");
     }
 }
+
