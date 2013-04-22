a123ca9c7e016130bbd6357d3f45e907aace6605
Frank Bennett <biercenator@gmail.com>
2013-04-22 09:00:19 +0900

    Fix a syntax error

--- Scannable Cite.js
+++ Scannable Cite.js
@@ -46,8 +46,7 @@ function doExport() {
         mem.setlaw(item.volume);
         mem.setlaw(item['container-title']);
         mem.setlaw(item.page);
-    }
-	var date = Zotero.Utilities.strToDate(item.date);
+	    var date = Zotero.Utilities.strToDate(item.date);
         var dateS = (date.year) ? date.year : item.date;
         Zotero.write(creatorsS + ", " + titleS + ", " + dateS + "| | |");
         Zotero.write("zotero://select/items/" + library_id + "_" + item.key + "}");
