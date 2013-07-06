7cd37f41ac0074857a0b5eace884c9adc0413e48
Frank Bennett <biercenator@gmail.com>
2013-07-06 23:14:10 +0900

    Remove debugging comment again -- update chain is working again after hosting service server upgrade

--- Uzbekistan lex.uz.js
+++ Uzbekistan lex.uz.js
@@ -116,7 +116,6 @@ function doWeb(doc, url) {
                     item.regulatoryBody = acceptingBody;
                 }
                 if (i === 0) {
-                    Zotero.debug("XXX try again");
                     item.attachments.push({url:url,title:title,mimeType:"text/html"});
                 }
                 item.complete();
