90f55546cb89a2e061a8c91ccc52ce684cf6df48
fbennett <biercenator@gmail.com>
2016-02-23 08:22:02 +0900

    Add license text

--- HeinOnline.js
+++ HeinOnline.js
@@ -13,7 +13,28 @@
 }
 
 /*
- *** Utility Functions ***
+   HeinOnline Translator
+   Copyright (C) 2016 CHNM, Frank Bennett
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Affero General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Affero General Public License for more details.
+
+   You should have received a copy of the GNU Affero General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+/*
+    ***************
+    ** Utilities **
+    ***************
 */
 
 // Get any search results from current page
@@ -78,7 +99,7 @@ function translatePage(ris, pdfURL) {
 	if (pdfURL) {
 		trans.setHandler('itemDone', function (obj, item) {
 			item.attachments = [{
-				title: "HeinOnline PDF",
+				title: "Full Text PDF",
 				url: pdfURL,
 				mimeType: "application/pdf"
 			}];
@@ -118,6 +139,12 @@ function scrapePage(doc, url) {
 	} , null , "UTF-8");
 
 }
+
+/*
+    *********
+    ** API **
+    *********
+*/
 
 function detectWeb (doc, url) {
 	if (url.indexOf("/LuceneSearch?") > -1) {
